26490a66a458a8c8e16cc1e290c24f390d8c287a
Jeka_V <zheka_v@inbox.ru>
2019-04-07 23:31:54 +0300

    Unit 2 lesson 3: Scheduler, Pub/Sub and Counters

--- src/Unit-2/DoThis/Main.Designer.cs
+++ src/Unit-2/DoThis/Main.Designer.cs
@@ -28,47 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.sysChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.AddSeriesButton = new System.Windows.Forms.Button();
+            this.cpuButton = new System.Windows.Forms.Button();
+            this.discButton = new System.Windows.Forms.Button();
+            this.memoryButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.sysChart)).BeginInit();
             this.SuspendLayout();
             // 
             // sysChart
             // 
-            chartArea2.Name = "ChartArea1";
-            this.sysChart.ChartAreas.Add(chartArea2);
+            chartArea4.Name = "ChartArea1";
+            this.sysChart.ChartAreas.Add(chartArea4);
             this.sysChart.Dock = System.Windows.Forms.DockStyle.Fill;
-            legend2.Name = "Legend1";
-            this.sysChart.Legends.Add(legend2);
+            legend4.Name = "Legend1";
+            this.sysChart.Legends.Add(legend4);
             this.sysChart.Location = new System.Drawing.Point(0, 0);
             this.sysChart.Name = "sysChart";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.sysChart.Series.Add(series2);
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.sysChart.Series.Add(series4);
             this.sysChart.Size = new System.Drawing.Size(684, 446);
             this.sysChart.TabIndex = 0;
             this.sysChart.Text = "sysChart";
             // 
-            // AddSeriesButton
+            // cpuButton
             // 
-            this.AddSeriesButton.Location = new System.Drawing.Point(571, 359);
-            this.AddSeriesButton.Name = "AddSeriesButton";
-            this.AddSeriesButton.Size = new System.Drawing.Size(101, 46);
-            this.AddSeriesButton.TabIndex = 1;
-            this.AddSeriesButton.Text = "Add series";
-            this.AddSeriesButton.UseVisualStyleBackColor = true;
-            this.AddSeriesButton.Click += new System.EventHandler(this.AddSeriesButton_Click);
+            this.cpuButton.Location = new System.Drawing.Point(576, 244);
+            this.cpuButton.Name = "cpuButton";
+            this.cpuButton.Size = new System.Drawing.Size(96, 50);
+            this.cpuButton.TabIndex = 3;
+            this.cpuButton.Text = "CPU (ON)";
+            this.cpuButton.UseVisualStyleBackColor = true;
+            this.cpuButton.Click += new System.EventHandler(this.CpuButton_Click);
+            // 
+            // discButton
+            // 
+            this.discButton.Location = new System.Drawing.Point(576, 356);
+            this.discButton.Name = "discButton";
+            this.discButton.Size = new System.Drawing.Size(96, 50);
+            this.discButton.TabIndex = 4;
+            this.discButton.Text = "DISC (OFF)";
+            this.discButton.UseVisualStyleBackColor = true;
+            this.discButton.Click += new System.EventHandler(this.DiscButton_Click);
+            // 
+            // memoryButton
+            // 
+            this.memoryButton.Location = new System.Drawing.Point(576, 300);
+            this.memoryButton.Name = "memoryButton";
+            this.memoryButton.Size = new System.Drawing.Size(96, 50);
+            this.memoryButton.TabIndex = 5;
+            this.memoryButton.Text = "MEMORY (OFF)";
+            this.memoryButton.UseVisualStyleBackColor = true;
+            this.memoryButton.Click += new System.EventHandler(this.MemoryButton_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 446);
-            this.Controls.Add(this.AddSeriesButton);
+            this.Controls.Add(this.memoryButton);
+            this.Controls.Add(this.discButton);
+            this.Controls.Add(this.cpuButton);
             this.Controls.Add(this.sysChart);
             this.Name = "Main";
             this.Text = "System Metrics";
@@ -82,7 +106,9 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart sysChart;
-        private System.Windows.Forms.Button AddSeriesButton;
+        private System.Windows.Forms.Button cpuButton;
+        private System.Windows.Forms.Button discButton;
+        private System.Windows.Forms.Button memoryButton;
     }
 }
 
--- src/Unit-2/DoThis/Main.cs
+++ src/Unit-2/DoThis/Main.cs
@@ -11,7 +11,8 @@ namespace ChartApp
     public partial class Main : Form
     {
         private IActorRef _chartActor;
-        private readonly AtomicCounter _seriesCounter = new AtomicCounter(1);
+        private IActorRef _coordinatorActor;
+        private readonly Dictionary<CounterType, IActorRef> _toggleActors = new Dictionary<CounterType, IActorRef>();
 
         public Main()
         {
@@ -24,11 +25,27 @@ namespace ChartApp
         private void Main_Load(object sender, EventArgs e)
         {
             _chartActor = Program.ChartActors.ActorOf(Props.Create(() => new ChartingActor(sysChart)), "charting");
-            var series = ChartDataHelper.RandomSeries("FakeSeries" + _seriesCounter.GetAndIncrement());
-            _chartActor.Tell(new ChartingActor.InitializeChart(new Dictionary<string, Series>()
-            {
-                {series.Name, series}
-            }));
+            _chartActor.Tell(new ChartingActor.InitializeChart(null)); //no initial series
+
+            _coordinatorActor = Program.ChartActors.ActorOf(Props.Create(() => new PerformanceCounterCoordinatorActor(_chartActor)), "counters");
+
+            // CPU button toggle actor
+            _toggleActors[CounterType.Cpu] = Program.ChartActors.ActorOf(
+                Props.Create(() => new ButtonToggleActor(_coordinatorActor, cpuButton, CounterType.Cpu, false))
+                .WithDispatcher("akka.actor.synchronized-dispatcher"));
+
+            // MEMORY button toggle actor
+            _toggleActors[CounterType.Memory] = Program.ChartActors.ActorOf(
+               Props.Create(() => new ButtonToggleActor(_coordinatorActor, memoryButton, CounterType.Memory, false))
+                .WithDispatcher("akka.actor.synchronized-dispatcher"));
+
+            // DISK button toggle actor
+            _toggleActors[CounterType.Disk] = Program.ChartActors.ActorOf(
+               Props.Create(() => new ButtonToggleActor(_coordinatorActor, discButton, CounterType.Disk, false))
+               .WithDispatcher("akka.actor.synchronized-dispatcher"));
+
+            // Set the CPU toggle to ON so we start getting some data
+            _toggleActors[CounterType.Cpu].Tell(new ButtonToggleActor.Toggle());
         }
 
         private void Main_FormClosing(object sender, FormClosingEventArgs e)
@@ -42,10 +59,19 @@ namespace ChartApp
 
         #endregion
 
-        private void AddSeriesButton_Click(object sender, EventArgs e)
+        private void CpuButton_Click(object sender, EventArgs e)
         {
-            var series = ChartDataHelper.RandomSeries("FakeSeries" + _seriesCounter.GetAndIncrement());
-            _chartActor.Tell(new ChartingActor.AddSeries(series));
+            _toggleActors[CounterType.Cpu].Tell(new ButtonToggleActor.Toggle());
+        }
+
+        private void MemoryButton_Click(object sender, EventArgs e)
+        {
+            _toggleActors[CounterType.Memory].Tell(new ButtonToggleActor.Toggle());
+        }
+
+        private void DiscButton_Click(object sender, EventArgs e)
+        {
+            _toggleActors[CounterType.Disk].Tell(new ButtonToggleActor.Toggle());
         }
     }
 }
